2cc130bd719626d4e177404ae11562d35fc9116f
Abhilash4597 <abhilash4597@gmail.com>
2023-02-06 01:47:11 +0530

    updated

--- script.js
+++ script.js
@@ -3,3 +3,6 @@
 // ! NOTE
 //* if want to right the code in web page and see the output than follow the below step
 //* inspect => sources => arrow => snippets
+
+// ! prompt("") is just like alert("") but it takes the input in alert box
+// * var yourName = prompt('what is your name?'); => the answer will be stored in variable
