b89a042647a47a2742a65959fd6396f5f56fba7c
Abhilash4597 <abhilash4597@gmail.com>
2023-02-09 13:31:27 +0530

    about constructor function

--- script.js
+++ script.js
@@ -7,5 +7,20 @@
 // ! prompt("") is just like alert("") but it takes the input in alert box
 // * var yourName = prompt('what is your name?'); => the answer will be stored in variable
 
-//# Date-8/2/23
+// ! confirm("") is also like alert but it returns the "true" if we click on OK and "false" when we click on cancel
+
+// * Whenever we are inspecting on particular element than we can access that element using [$0] in console for manipulating if needed.
+
+// ? 'CONSTRUCTOR FUNCTION':- The starting letter of the function name is also capital which helps us to recognize that this function is constructor function. This function helps to initialise new object as many we want.
+// todo:- Example
+
+function Example(name,age,study,languages){
+  this.name = name;
+  this.age = age;
+  this.study = study;
+  this.languages = languages;
+}
+// ? => Initialising Object.
+let obj = new Example('Lucifer', 25, 'Graduated', ['English', 'Hindi']);
+
 
