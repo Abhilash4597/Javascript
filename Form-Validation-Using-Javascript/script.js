bfe1ffbaeb121dd6c3222d27e50b33d47f02d470
Abhilash4597 <abhilash4597@gmail.com>
2023-03-05 14:25:53 +0530

    updated

--- Form-Validation-Using-Javascript/script.js
+++ Form-Validation-Using-Javascript/script.js
@@ -1,1 +1,10 @@
 'use strict'
+
+// let messH1 = document.querySelector('.mesH1');
+// console.log(messH1.innerHTML = `<h1>hello</h1>`)
+// messH1.innerHTML = `<p>Hello ${name.value}</p>`;
+let name = document.querySelector('#name');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+let cpassword = document.querySelector('#cpassword');
+let phone = document.querySelector('#phone');
