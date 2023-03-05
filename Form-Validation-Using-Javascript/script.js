ff123a4c1c1b42f99a431e32a267a9833639911f
Abhilash4597 <abhilash4597@gmail.com>
2023-03-05 15:59:53 +0530

    updated

--- Form-Validation-Using-Javascript/script.js
+++ Form-Validation-Using-Javascript/script.js
@@ -3,22 +3,19 @@
 // let messH1 = document.querySelector('.mesH1');
 // console.log(messH1.innerHTML = `<h1>hello</h1>`)
 // messH1.innerHTML = `<p>Hello ${name.value}</p>`;
-let name = document.querySelector('#name');
-let email = document.querySelector('#email');
-let password = document.querySelector('#password');
-let cpassword = document.querySelector('#cpassword');
-let phone = document.querySelector('#phone');
 
 function validate() {
-  if (password.value.trim().length >= 12) {
-    alert('length is so long');
+  let password = document.querySelector('#password');
+  let name = document.querySelector('#name');
+  let email = document.querySelector('#email');
+  let cpassword = document.querySelector('#cpassword');
+  let phone = document.querySelector('#phone');
+  
+  if (cpassword.value !== password.value) {
+    alert('please write correct password');
     return false;
-  } else if (cpassword.value.trim() !== password.value.trim()) {
-    alert('please write password');
-    return false;
-  } else if (phone.value.length !== 10) {
+  } else if (phone.value.trim().length!==10) {
     alert('please write correct number');
     return false;
   }
-  return true;
 }
