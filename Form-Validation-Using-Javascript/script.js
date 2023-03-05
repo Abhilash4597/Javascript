1684dbcad56b3f7665a8b883299f788a399dec25
Abhilash4597 <abhilash4597@gmail.com>
2023-03-05 15:02:48 +0530

    updated

--- Form-Validation-Using-Javascript/script.js
+++ Form-Validation-Using-Javascript/script.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // let messH1 = document.querySelector('.mesH1');
 // console.log(messH1.innerHTML = `<h1>hello</h1>`)
@@ -8,3 +8,17 @@ let email = document.querySelector('#email');
 let password = document.querySelector('#password');
 let cpassword = document.querySelector('#cpassword');
 let phone = document.querySelector('#phone');
+
+function validate() {
+  if (password.value.trim().length >= 12) {
+    alert('length is so long');
+    return false;
+  } else if (cpassword.value.trim() !== password.value.trim()) {
+    alert('please write password');
+    return false;
+  } else if (phone.value.length !== 10) {
+    alert('please write correct number');
+    return false;
+  }
+  return true;
+}
