2ef393135f0f793fd8ea5ce6637f9e1fa6b3245c
Abhilash4597 <abhilash4597@gmail.com>
2023-03-29 20:41:07 +0530

    SQL notes updated

--- SQL notes/notes.sql
+++ SQL notes/notes.sql
@@ -86,3 +86,17 @@
 --todo:-             WHERE NOT condition;
 
 
+-- ! INSERT INTO Syntax
+--* The INSERT INTO statement is used to insert new records in a table.
+
+-- # It is possible to write the INSERT INTO statement in two ways:
+
+-- ? 1. Specify both the column names and the values to be inserted:
+--todo:-         INSERT INTO table_name (column1, column2, column3, ...)
+--todo:-         VALUES (value1, value2, value3, ...);
+
+-- ? 2. If you are adding values for all the columns of the table, you do not need to specify the column names in the SQL query. However, make sure the order of the values is in the same order as the columns in the table. Here, the INSERT INTO syntax would be as follows:
+--todo:-         INSERT INTO table_name
+--todo:-         VALUES (value1, value2, value3, ...);
+
+-- !NOTE:- In the above value should be written as per there DataType was described (eg:-(1,'apple',20.5))
