ea04de7d42c3e1bd077b0e9ae6a822262bc45dd7
Abhilash4597 <abhilash4597@gmail.com>
2023-03-29 13:38:58 +0530

    SQL notes updated

---
+++ SQL notes/notes.sql
@@ -0,0 +1,37 @@
+-- # What is SQL ?
+-- * SQL stands for Structured Query Language.
+-- * SQL lets you access and manipulate databases.
+
+-- # What Can SQL do ?
+-- * SQL can execute queries against a database.
+-- * SQL can retrieve data from a database.
+-- * SQL can insert records in a database.
+-- * SQL can update records in a database.
+-- * SQL can delete records from a database.
+-- * SQL can create new databases.
+-- * SQL can create new tables in a database.
+-- * SQL can create stored procedures in a database.
+-- * SQL can create views in a database.
+-- * SQL can set permissions on tables, procedures, and views.
+
+-- # Some of The Most Important SQL Commands
+-- * SELECT - extracts data from a database
+-- * UPDATE - updates data in a database
+-- * DELETE - deletes data from a database
+-- * INSERT INTO - inserts new data into a database
+-- * CREATE DATABASE - creates a new database
+-- * ALTER DATABASE - modifies a database
+-- * CREATE TABLE - creates a new table
+-- * ALTER TABLE - modifies a table
+-- * DROP TABLE - deletes a table
+-- * CREATE INDEX - creates an index (search key)
+-- * DROP INDEX - deletes an index
+
+
+-- ! SELECT Syntax :-
+
+-- ? SELECT column1, column2, ... FROM table_name;
+-- *                       Here, column1, column2, ... are the field names of the table you want to select data from.
+
+-- todo- If you want to select all the fields available in the table, use the following syntax:
+-- ? SELECT * FROM table_name;
