00eb9b1d29a8175d63a304b3ebfd1e27635b3946
Abhilash4597 <abhilash4597@gmail.com>
2023-03-29 14:12:50 +0530

    SQL notes updated

--- SQL notes/notes.sql
+++ SQL notes/notes.sql
@@ -31,7 +31,36 @@
 -- ! SELECT Syntax :-
 
 -- ? SELECT column1, column2, ... FROM table_name;
--- *                       Here, column1, column2, ... are the field names of the table you want to select data from.
+-- *                                          Here, column1, column2, ... are the field names of the table you want to select data from.
 
 -- todo- If you want to select all the fields available in the table, use the following syntax:
+
 -- ? SELECT * FROM table_name;
+
+-- ! WHERE Syntax :-
+-- ? SELECT column1, column2, ... FROM table_name WHERE condition;
+                    -- * above and below both the syntax is right 
+-- ? SELECT column1, column2, ...
+-- ? FROM table_name
+-- ? WHERE condition;
+
+-- * The 'WHERE' clause is used to filter records. It is used to extract only those records that fulfill a specified condition.
+
+-- todo:- Eg:- The following SQL statement selects all the customers from the country "Mexico", in the "Customers" table:
+-- ?            SELECT * FROM Customers
+-- ?            WHERE Country='Mexico';
+
+-- ! Note: The WHERE clause is not only used in SELECT statements, it is also used in UPDATE, DELETE, etc.!
+
+-- # The following operators can be used in the WHERE clause:
+
+--? Operator	        Description	
+-- * =	              Equal	
+-- * >	              Greater than	
+-- * <	              Less than	
+-- * >=	              Greater than or equal	
+-- * <=	              Less than or equal	
+-- * <>	              Not equal. Note: In some versions of SQL this operator may be written as !=	
+-- * BETWEEN	        Between a certain range	
+-- * LIKE	            Search for a pattern	
+-- * IN	              To specify multiple possible values for a column
