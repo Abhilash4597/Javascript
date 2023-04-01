1017f7cb54c072cb53025e854a45acfa052a57a1
Abhilash4597 <abhilash4597@gmail.com>
2023-04-02 01:10:09 +0530

    header and footer of keeper app done

--- React notes/src/App.jsx
+++ React notes/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 // import StylePractice from './Practice Components/StylePractice';
 import './App.css';
+import Header from './Keeper App/Header';
+import Footer from './Keeper App/Footer';
 
 function App() {
   return (
     <>
+      <Header />
+
+      
+      <Footer />
       {/* <StylePractice></StylePractice> */}
     </>
   );
