41336210103702775d467deb722c333d6bdc1a3c
Abhilash4597 <abhilash4597@gmail.com>
2023-04-02 01:39:25 +0530

    data rendering and folder structure update

--- React notes/src/App.jsx
+++ React notes/src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-// import StylePractice from './Practice Components/StylePractice';
-import './App.css';
-import Header from './Keeper App/Header';
-import Footer from './Keeper App/Footer';
+import './keeper.css';
+import Header from './Keeper-App/Header';
+import Footer from './Keeper-App/Footer';
+import Note from './Keeper-App/Note';
+import data from './Keeper-App/Data';
 
 function App() {
   return (
     <>
       <Header />
-
-      
+      <Note data={ data } />
       <Footer />
-      {/* <StylePractice></StylePractice> */}
     </>
   );
 }
