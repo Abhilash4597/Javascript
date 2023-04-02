1d77c8f95b4ad48f59f205b3c71263e06f08cf82
Abhilash4597 <abhilash4597@gmail.com>
2023-04-02 19:59:31 +0530

    watch using useState

--- React notes/src/useState-Practice/CurrentTime.jsx
+++ React notes/src/useState-Practice/CurrentTime.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../useState.css';
 
 export default function CurrentTime() {
+  // # Current Time
   let newTime = new Date();
   let curTime =
     newTime.getHours() +
@@ -9,9 +10,10 @@ export default function CurrentTime() {
     newTime.getMinutes() +
     ':' +
     newTime.getSeconds();
-
+// # UseState
   const [time, setTime] = useState(curTime);
 
+  // # Current Time Function for set the time
   const refreshTime = () => {
     let updateTime = new Date();
     let newUpdateTime =
@@ -22,6 +24,9 @@ export default function CurrentTime() {
       updateTime.getSeconds();
     setTime(newUpdateTime);
   };
+
+  // # Continous working clock without refreshing
+  setInterval(refreshTime, 1000);
 
   return (
     <div className="container">
