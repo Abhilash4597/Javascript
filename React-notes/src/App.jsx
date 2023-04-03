8896000ff87d0634ea951aa4d4520a9c4588b711
Abhilash4597 <abhilash4597@gmail.com>
2023-04-03 17:57:08 +0530

    Updated folder name

--- React-notes/src/App.jsx
+++ React-notes/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ComplexStatePractice from './Event-handling/ComplexStatePractice';
+import ComplexStatePractice from './Event-state-handling/ComplexStatePractice';
 // import Header from './Keeper-App/Header';
 // import Footer from './Keeper-App/Footer';
 // import Note from './Keeper-App/Note';
