6f5f262489852dac9b21bd2c32b352c37e691681
Abhilash4597 <abhilash4597@gmail.com>
2023-04-03 17:47:00 +0530

    Complex state more practice using form

--- React-notes/src/App.jsx
+++ React-notes/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ComplexState from './Event-handling/ComplexState';
+import ComplexStatePractice from './Event-handling/ComplexStatePractice';
 // import Header from './Keeper-App/Header';
 // import Footer from './Keeper-App/Footer';
 // import Note from './Keeper-App/Note';
 // import data from './Keeper-App/Data';
-// import HandleEvent from './Event-handling/HandleEvent';
-// import FormEvent from './Event-handling/FormEvent';
 
 function App() {
 
@@ -14,8 +12,9 @@ function App() {
       {/* <Header />
       <Note data={ data } />
       <Footer /> */}
-      {/* <HandleEvent /> */}
-      <ComplexState />
+      {/* <HandleEvent />
+      <ComplexState /> */}
+      <ComplexStatePractice />
     </>
   );
 }
---
+++ React-notes/src/Event-handling/ComplexStatePractice.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './HandleEvent.css';
+
+export default function ComplexStatePractice() {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contact, setContact] = useState({
+    fName: '',
+    lName: '',
+    email: '',
+  });
+
+  function handleContact(e) {
+    const {name, value} = e.target;
+
+    setContact(preValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: preValue.lName,
+          email: preValue.email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: preValue.fName,
+          lName: value,
+          email: preValue.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: preValue.fName,
+          lName: preValue.lName,
+          email: value,
+        };
+      }
+    });
+  }
+
+  // # On form submission 
+
+  function handleClick(e) {
+    e.preventDefault()
+    setFullName(contact.fName + ' ' + contact.lName);
+    setEmail(contact.email);
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName}
+      </h1>
+      <p>{email}</p>
+      <form onSubmit={handleClick}>
+        <input name="fName" placeholder="First Name" onChange={handleContact} />
+        <input name="lName" placeholder="Last Name" onChange={handleContact} />
+        <input name="email" placeholder="Email" onChange={handleContact} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
