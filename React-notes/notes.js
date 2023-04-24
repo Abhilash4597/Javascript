d400b4d4a0db4b75c95962c47808d89a0f09f551
Abhilash4597 <abhilash4597@gmail.com>
2023-04-24 22:13:53 +0530

    updated react note

---
+++ React-notes/notes.js
@@ -0,0 +1,38 @@
+//React interview question....
+//? 1. What is React ?
+  //*  A JavaScript library created by Facebook for building user interfaces
+  //*  Its aim is to allow developers to easily create fast user interfaces for websites and applications 
+
+//? 2. Features of React ?
+  //*  (JC OV SP)JSX,Components,one way data binding,Virtual Dom,Simplicity,Performance
+  //*  JSX -JSX stands for JavaScript XML.
+  //*       JSX allows us to write HTML in React.
+  //*       JSX makes it easier to write and add HTML in React.
+        
+  //?  Components-Components are independent and they are reusable bits of code.
+  //?             Components are of two types, Class components and Function components
+  //?             Class Components:
+  //?             A class component must include the extends React.Component statement.
+  //?             The component also requires a render() method, this method returns HTML.
+              // Ex:- class Car extends React.Component {
+              //      render() {
+              //                 return <h2>Hi, I am a Car!</h2>;
+              //                }
+                  //  }
+              //? Function Components:
+              //* Function components can be written using much less code, are easier to understand
+              // Ex:- function Car() {
+              //        return <h2>Hi, I am a Car!</h2>;
+              //      }
+                   
+  //*  One-way Data Binding- it describes a one-way data flow where the data flows from the parent to the child component, it is known as
+  // *                       the unidirectional flow of data.
+                         
+  //*  Virtual DOM- The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory
+  //*               and synced with the “real” DOM by a library such as ReactDOM.
+  // *              the term “virtual DOM” is usually associated with React elements 
+               
+  // ? Simplicity- ReactJS uses JSX file which makes the application simple and to code as well as understand
+  // ?             ReactJS is a component-based approach which makes the code reusable as your need. This makes it simple to use and learn.           
+  
+  //*  Performance-React allows us to measure the performance of our apps using the Profiler in the React DevTools. 
