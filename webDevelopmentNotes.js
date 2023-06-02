15644967a8fa01b5be380b6515254524d16fa915
Abhilash4597 <abhilash4597@gmail.com>
2023-06-02 16:29:41 +0530

    update

--- webDevelopmentNotes.js
+++ webDevelopmentNotes.js
@@ -1,5 +1,8 @@
 // ! WEB DEVELOPMENT BY LOVE BABBAR :-
 
+//? WEB :-
+// * Web is a system where our Interconnected Document or Resources or Media files Exist and all this can be accessed using Internet.
+
 //? WEB DEVELOPMENT :-
 //* It is a process by using which we can create WEBSITES and WEB APPLICATIONS.
 //* Selection of Websites or Web Application is totally depends on our use case.
@@ -16,3 +19,24 @@
 //todo :- 2) BackEnd => Nodejs => It is also called as Brain of Web Application.
 //todo :- 3) DB (DataBase) Layer => MongoDB(NoSQL/SQL).
 //# All the above Three points together are known as Full Stack Web Development.
+
+// ? NETWORK :-
+// * Bunch of resources like(computer/printer/mobile etc) are connected with each other or can be connected with each other and the system where all this resources exist in a connected form is known as a NETWORK.
+
+//todo :- 1) LAN (Local Area Network) :-
+// * A local area network(LAN) is a collection of devices connected together in one physical location, such as a building, office, or home.
+// * A LAN can be small or large, ranging from a home network with one user to an enterprise network with thousands of users and devices in an office or school.
+
+//todo :- 2) MAN (Metropolitan Area Network) :-
+// * A metropolitan area network (MAN) is a computer network that connects computers within a metropolitan area, which could be a single large city, multiple cities and towns, or any given large area with multiple buildings.
+// * A MAN is larger than a local area network (LAN) but smaller than a wide area network (WAN).
+// * MANs do not have to be in urban areas; the term "metropolitan" implies the size of the network, not the demographics of the area that it serves.
+
+//todo :- 3) WAN (Wide-Area Network) :-
+// * A wide-area network (WAN) is a collection of local-area networks (LANs) or other networks that communicate with one another.
+// * A WAN is essentially a network of networks.
+// * A wide area network (WAN) is a geographically distributed private telecommunications network that interconnects multiple local area networks (LANs).
+
+
+
+
