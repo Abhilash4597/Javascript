df7e51a02661e1f20222740c07b1f4399a0f1ac2
Abhilash4597 <abhilash4597@gmail.com>
2023-06-02 15:53:42 +0530

    update about web development

---
+++ webDevelopmentNotes.js
@@ -0,0 +1,18 @@
+// ! WEB DEVELOPMENT BY LOVE BABBAR :-
+
+//? WEB DEVELOPMENT :-
+                   //* It is a process by using which we can create WEBSITES and WEB APPLICATIONS.
+                   //* Selection of Websites or Web Application is totally depends on our use case.  
+                
+// ? WEBSITES :-
+              //* It is a static kind of content.
+              //* It is in a read-only type of format like (Blogs/Wikipedia).
+              //* In this user interaction is near minimum or negligible.
+              
+// ? WEB APPLICATIONS :-
+                    //* It is a Dynamic kind of content.
+                    //* In this user interaction is at maximum level.
+                    //todo :- 1) FrontEnd/ UI (User Interface) => HTML/CSS/Javascript.
+                    //todo :- 2) BackEnd => Nodejs => It is also called as Brain of Web Application.
+                    //todo :- 3) DB (DataBase) Layer => MongoDB(NoSQL/SQL).
+                    //# All the above Three points together are known as Full Stack Web Development.
