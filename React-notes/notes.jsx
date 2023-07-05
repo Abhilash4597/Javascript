e9cad1d9d1eb69f9eca45798f8954a24a3152d2c
Abhilash4597 <abhilash4597@gmail.com>
2023-07-05 21:25:11 +0530

    update

--- React-notes/notes.jsx
+++ React-notes/notes.jsx
@@ -249,27 +249,27 @@
 // * every value in the style object should be written as String.
 // * For writing the style we have to follow the camel casing.(eg:-fontSize,backGroundColor)
 
-function App() {
-  const name = 'Abhilash';
-  const time = new Date();
-  const year = time.getFullYear();
+// function App() {
+//   const name = 'Abhilash';
+//   const time = new Date();
+//   const year = time.getFullYear();
 
-  const customCSS = {
-    color: 'red',
-    fontSize: '2rem',
-    backgroundColor: 'black',
-    border: '1px solid black',
-  };
+//   const customCSS = {
+//     color: 'red',
+//     fontSize: '2rem',
+//     backgroundColor: 'black',
+//     border: '1px solid black',
+//   };
 
-  return (
-    <div className="App">
-      <p style={customCSS}>Created by {name}</p> //* make a obj of styling ad
-      use it like this.
-      <p style={{ color: 'green' }}>Copyright {year}</p> //* OR use like this
-      directly
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <p style={customCSS}>Created by {name}</p> //* make a obj of styling ad
+//       use it like this.
+//       <p style={{ color: 'green' }}>Copyright {year}</p> //* OR use like this
+//       directly
+//     </div>
+//   );
+// }
 
 // # External Styling
 
