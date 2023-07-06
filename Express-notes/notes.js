a9845a3be1b62d11dcea19ebe33c5c79fee216ea
Abhilash4597 <abhilash4597@gmail.com>
2023-07-07 00:25:43 +0530

    update

--- Express-notes/notes.js
+++ Express-notes/notes.js
@@ -28,35 +28,35 @@ app.listen(3000, () => {
 // #  npm install -g nodemon  is used to auto detect the changes which happen in our directory and show on browser
 // # nodemon server.js  is used to start nodemon
 
-// res.send  is used to send the direct msg
+//* res.send  is used to send the direct msg
 
-// res.sendFile  is used to send the whole file which we want to send to the browser
-// eg :-  res.sendFile(index.html)  this will find the file in its folder and show to user but this will work for local system only so
+//* res.sendFile  is used to send the whole file which we want to send to the browser
+//? eg :-  res.sendFile(index.html)  this will find the file in its folder and show to user but this will work for local system only so
 
-// res.sendFile(__dirname + "/index.html")  using like this it will show the whole path correctly anywhere or in all servers and clouds
+//* res.sendFile(__dirname + "/index.html")  using like this it will show the whole path correctly anywhere or in all servers and clouds
 
-// __dirname  gives the perfect path of the directory at which we are right now
+//* __dirname  gives the perfect path of the directory at which we are right now
 
-// in form action defines the path where we want to send the data or get the data according to the method of the same form
+//* in form action defines the path where we want to send the data or get the data according to the method of the same form
 
-// As the action of form defines the path so for getting the data from that path we have to use post method in express
+//* As the action of form defines the path so for getting the data from that path we have to use post method in express
 
-// eg :- app.post('/',(req,res)=>{
-// res.send('hello);
-// })
+//? eg :- app.post('/',(req,res)=>{
+//? res.send('hello);
+//? })
 
-// For getting the data og input of the forms we have to install
-// npm install body-parser
+//* For getting the data og input of the forms we have to install
+//* npm install body-parser
 
-// and after installing we have to require it like a express
-// const bodyParser = require('body-parser')
+//* and after installing we have to require it like a express
+//* const bodyParser = require('body-parser')
 
-// and after that we have to use this everytime to get the data prperly
+//* and after that we have to use this everytime to get the data prperly
 
-// app.use(bodyParser.urlencoded({ extended: true }))
+//* app.use(bodyParser.urlencoded({ extended: true }))
 
-// after this we can use the data in post function as
-// req.body.(whatever the name given in name attributes we have to right here as the data we are getting as a object and the individual value which we are getting is as a text so for number we have to change the value into Number)
+//* after this we can use the data in post function as
+//* req.body.(whatever the name given in name attributes we have to right here as the data we are getting as a object and the individual value which we are getting is as a text so for number we have to change the value into Number)
 
 app.get('/bmicalculator', (req, res) => {
   res.sendFile(__dirname + '/bmicalculator.html');
